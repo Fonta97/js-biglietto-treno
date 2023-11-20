1a861bdd51b8fda62caf53ebc83b6db4bce3f7f5
Fonta97 <fontana.alessandro97@gmail.com>
2023-11-20 10:08:15 +0100

    Ottimizzazione dopo correzione

--- js/script.js
+++ js/script.js
@@ -1,25 +1,24 @@
 // richiesta input ad utente su lunghezza viaggio e età
-const wthIsAKilometer = prompt("Quant'è la lunghezza del tuo viaggio in Km?");
-const userAge = prompt("Quanti anni hai?");
+const wthIsAKilometer = parseInt(prompt("Quant'è la lunghezza del tuo viaggio in Km?"));
+const userAge = parseInt(prompt("Quanti anni hai?"));
 
-console.log(wthIsAKilometer, userAge)
+console.log(wthIsAKilometer, userAge);
 
 
 // dichiarazione variabili costanti prezzo per km e scontistiche in base all'età
-let priceKm = (wthIsAKilometer * 0.21)
-let discount
+let priceKm = wthIsAKilometer * 0.21;
+let finalPrice
+// calcolo sconto se presente
 if (userAge < 18) {
-    discount = 0.8
+    finalPrice = wthIsAKilometer * 0.8
 } else if (userAge > 65) {
-    discount = 0.6
-} else {
-    discount = 1
+    finalPrice = wthIsAKilometer * 0.6
 }
 
-console.log(priceKm, discount)
+console.log(priceKm, finalPrice)
 
-let finalPrice = parseFloat(priceKm * discount).toFixed(2)
+finalPrice = (priceKm).toFixed(2)
 
 console.log(finalPrice)
 
-document.getElementById("ticket_price").innerHTML = "PAGAH:" + finalPrice + "€";
+document.getElementById("ticket_price").innerHTML = "Paga o il trenino Thomas ti verrà a prendere di notte:" + finalPrice + "€";
